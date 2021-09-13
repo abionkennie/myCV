2e1902d670c08e9d793dd4171990dd7dbe908de2
abionkennie <abionkennie@gmail.com>
2021-09-13 15:03:02 +0100

    updated js script

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const Gallery = document.querySelector("#gallery .box-container")
-const hidePics = document.forms('hide-pics')
-hidePics.addEventListener('change', function(e) {
-    if (hidePics.checked) {
-        list.style.display = "none";
-    } else {
-        list.style.display = "block";
-    }
-})
+const btnP = document.querySelectorAll('.btn');
+
+btnP.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        var btnId = e.target.id;
+        var pTag = document.querySelector("#" + btnId + "-info-tag").innerHTML;
+        alert(pTag)
+    });
+});
